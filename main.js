4a2682eb2bfc8ebb4d8cba931ac948172bf9b11a
IvetteMery <ivette.mery@gmail.com>
2020-08-11 17:07:15 -0400

    lista-de-reproduccion

---
+++ main.js
@@ -0,0 +1,3 @@
+alert("Para poder crear tu playlist por favor ingresa el titulo de 3 canciones")
+let playlist = prompt("Primera cancion") + " ' , ' " + prompt("Segunda cancion") + " ',  ' " + prompt("Tercera cancion") 
+alert ( ` Se a creado la playlist  "Hits de los 80s'"  con las canciones ' ${playlist} ' .`)
